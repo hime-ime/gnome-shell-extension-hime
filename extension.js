2dfc8925a8a45baee5fd35f8ce140bbb66c9f9ca
Dieter Hsu <dieterplex@gmail.com>
2012-01-09 07:22:57 +0800

    simple impl.

---
+++ extension.js
@@ -0,0 +1,9 @@
+//http://blog.fpmurphy.com/2011/05/more-gnome-shell-customization.html 
+
+const Panel = imports.ui.panel;
+const StatusIconDispatcher = imports.ui.statusIconDispatcher;
+
+function init() {
+    StatusIconDispatcher.STANDARD_TRAY_ICON_IMPLEMENTATIONS['hime'] = 'hime';
+}
+
